7763bfada7330f29efc41e9792ae749467dbfefe
Ryan <pnugraha89@gmail.com>
2019-11-23 10:06:52 +1100

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import UseForm, {propTypes as formPropTypes, defaultProps as formDefaultProps} from './useForm';
+import UseForm, { propTypes as formPropTypes, defaultProps as formDefaultProps } from './useForm';
 
 const FormStateManager = props => {
   const { stateSchema, validationSchema, callback } = props;
 
-  return (
-    <UseForm stateSchema={stateSchema} validationSchema={validationSchema} callback={callback} />
-  );
+  return UseForm({ stateSchema: stateSchema, validationSchema: validationSchema, callback: callback });
 };
 
 FormStateManager.propTypes = formPropTypes
